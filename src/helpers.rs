162f4669da19fc7696267c274dea7e65f14de92b
Kevin Ngo <quocdatngo2003@gmail.com>
2025-11-20 20:59:00 +1100

    Add helper functions to avoid repeated codes

--- src/helpers.rs
+++ src/helpers.rs
@@ -28,3 +28,34 @@ pub fn construct_cell(cell_identifier: CellIdentifier) -> String {
     let cell_number = (cell_identifier.row + 1).to_string();
     format!("{}{}", cell_character, cell_number)
 }
+
+pub fn get_cell_value_or_error(sheet: &RSheet, cell_name: &str) -> CellValue {
+    match sheet.get(&cell_name.to_string()) {
+        CellArgument::Value(e) => e,
+        _ => CellValue::Error("Cell does not contain a value".to_string()),
+    }
+}
+
+pub fn build_vector(sheet: &RSheet, col_start: u32, col_end: u32, row: u32) -> Vec<CellValue> {
+    (col_start..=col_end)
+        .map(|i| {
+            let cell_name = format!("{}{}", column_number_to_name(i), row);
+            get_cell_value_or_error(sheet, &cell_name)
+        })
+        .collect()
+}
+
+pub fn build_vector_by_row(sheet: &RSheet, col: u32, row_start: u32, row_end: u32) -> Vec<CellValue> {
+    (row_start..=row_end)
+        .map(|i| {
+            let cell_name = format!("{}{}", column_number_to_name(col), i);
+            get_cell_value_or_error(sheet, &cell_name)
+        })
+        .collect()
+}
+
+pub fn build_matrix(sheet: &RSheet, col_start: u32, col_end: u32, row_start: u32, row_end: u32) -> Vec<Vec<CellValue>> {
+    (row_start..=row_end)
+        .map(|i| build_vector(sheet, col_start, col_end, i))
+        .collect()
+}
